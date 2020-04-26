844489d9833f19d2a324e940d8449d7b9ebbd530
arfath77 <arfathtade@gmail.com>
2020-04-27 01:30:03 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
+
+class App extends React.Component {
+  state = { lat : null, errorMessage : '' }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {this.setState({lat : position.coords.latitude })},
+      err => {this.setState({errorMessage : err.message})}
+    )
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat){
+      return (
+        <div>Error Message : {this.state.errorMessage}</div>
+      )
+    }
+    if (!this.state.errorMessage && this.state.lat){
+      return (
+        <SeasonDisplay lat = {this.state.lat} />
+      )
+    }
+    return (
+      <Loader message="Please allow the loction to be detected"/>
+    )
+  }
+  render () {
+    return (<div>
+      {this.renderContent()}
+    </div>)
+  }    
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
